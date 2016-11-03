eaf18093af704b38fe868b50cc7e1c4289b6e364
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2016-11-03 18:28:34 -0500

    added example of defining a custom ESLint rule

---
+++ eslint/bad-code.js
@@ -0,0 +1,13 @@
+function isOdd(n) {
+  return n % 2 ? true : false;
+  /*
+  if (n % 2) {
+    return true;
+  } else {
+    return false;
+  }
+  */
+}
+
+console.log('isOdd(1) =', isOdd(1));
+console.log('isOdd(2) =', isOdd(2));
