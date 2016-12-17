7f11ed1e460f89055661392744c3c34d383256c3
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2016-12-16 19:35:34 -0600

    let to const in one file

--- es6/arrow-functions/arrow-functions6.js
+++ es6/arrow-functions/arrow-functions6.js
@@ -1,19 +1,19 @@
 'use strict';
 /*jshint esnext: true */
 
-let arr = [1, 2, 3, 4];
-let doubled = arr.map(x => x * 2);
+const arr = [1, 2, 3, 4];
+const doubled = arr.map(x => x * 2);
 console.log(doubled);
 
-let product = (a, b) => a * b;
+const product = (a, b) => a * b;
 console.log('name of function is', product.name); // product
 console.log(product(2, 3));
 
-let average = numbers => {
-  let sum = numbers.reduce((a, b) => a + b);
+const average = numbers => {
+  const sum = numbers.reduce((a, b) => a + b);
   return sum / numbers.length;
 };
 console.log(average(arr));
 
-let getObj = () => ({foo: 1, bar: 2});
+const getObj = () => ({foo: 1, bar: 2});
 console.log(getObj());
